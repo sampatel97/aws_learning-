da66fbf767eb1c9c325253f6ecad87b4d552c896
sampatel97 <61437312+sampatel97@users.noreply.github.com>
2022-12-18 13:15:31 -0600

    Update question1.sql

--- question1.sql
+++ question1.sql
@@ -22,6 +22,19 @@ WHERE hiredate BETWEEN '1981-01-01' AND '1981-12-31';
 
 -- 6. List the Empno, Ename, Sal, Daily Sal of all Employees in the ASC order of AnnSal. (Note devide sal/30 as annsal)
 
+SELECT empno, ename, sal, sal/30 AS daily_sal, 12*sal AS annual_salary
+FROM cards_ingest.emp
+ORDER BY annual_salary ASC;
+
 -- 7. List the employees who are working for the department name ACCOUNTING
 
+SELECT * FROM cards_ingest.emp 
+WHERE deptno IN
+(SELECT deptno from cards_ingest.dept WHERE dname = 'ACCOUNTING');
+
 -- 8. List the employees who does not belong to department name ACCOUNTING
+
+SELECT * FROM cards_ingest.emp 
+WHERE deptno IN
+(SELECT deptno from cards_ingest.dept WHERE NOT dname = 'ACCOUNTING');
+
